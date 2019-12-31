73ba0df3ce0f90010d7b5be9556157dcda533e0a
KipCrossing <kip.crossing@gmail.com>
2019-12-31 15:54:59 +1100

    Update 2020

--- kipstart.sh
+++ kipstart.sh
@@ -1,30 +1,21 @@
 #!/usr/bin/env bash
 
+apt-get update
+apt-get upgrade -y
+
+apt-get install terminator -y
+apt-get install git -y
+apt-get install chromium -y
+apt-get install python3-pip -y
+apt-get install wget
+
+echo 'alias python=python3' >> ~/.bashrc
+echo 'alias pip=pip3' >> ~/.bashrc
+
+source ~/.bashrc
+
+#Atom
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
-sudo apt-get upgrade -y
-
-sudo apt-get install git-all
-
-#Install pip
-sudo apt-get install python-setuptools python-dev build-essential
-sudo easy_install pip 
-sudo pip install --upgrade virtualenv 
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-
-sudo add-apt-repository ppa:gnome-terminator
-sudo add-apt-repository ppa:kivy-team/kivy
-
-sudo apt-get update 
-
-sudo apt-get install google-chrome-stable -y
-sudo apt-get install terminator -y
-sudo apt-get install python-kivy -y
-
-pip install --upgrade buildozer
-sudo pip install --upgrade cython==0.21
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install build-essential ccache git libncurses5:i386 libstdc++6:i386 libgtk2.0-0:i386 libpangox-1.0-0:i386 libpangoxft-1.0-0:i386 libidn11:i386 python2.7 python2.7-dev openjdk-7-jdk unzip zlib1g-dev zlib1g:i386
+sudo apt-get install atom -y
