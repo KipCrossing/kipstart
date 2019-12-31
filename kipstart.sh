f91db130f2b913e067a0cdbde24d234c71710591
KipCrossing <kip.crossing@gmail.com>
2019-12-31 17:04:22 +1100

    Atom packages

--- kipstart.sh
+++ kipstart.sh
@@ -19,3 +19,28 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
 sudo apt-get install atom -y
+
+sudo apt install hunspell-en-au
+
+# For Python PEP8
+apm install linter
+pip3 install pep8
+pip3 install pycodestyle
+apm install linter-pycodestyle
+pip3 install autopep8
+apm install python-autopep8
+# (tick format on save)
+
+apm install linter-markdown
+apm install tidy-markdown
+apm install markdown-preview-plus
+# Ctrl+Shift+m to run
+
+# This is a nice touch
+apm install file-icons
+apm install atom-ide-terminal
+
+# Discord
+wget -O discord-0.0.1.deb https://discordapp.com/api/download?platform=linux&format=deb
+sudo dpkg -i discord-0.0.1.deb
+apt --fix-broken install -y
