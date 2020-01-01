37cc47a42ef4058d0da7aa58f29ebfbfbeaf5623
KipCrossing <kip.crossing@gmail.com>
2020-01-01 19:56:30 +1100

    install pyqt5-dev-tools

--- installqgis.sh
+++ installqgis.sh
@@ -3,4 +3,10 @@ echo 'deb-src https://qgis.org/debian buster main' >> /etc/apt/sources.list
 apt update
 apt install qgis python3-qgis qgis-plugin-grass
 apt-get install python-qt4
-
+sudo apt-get update
+sudo apt-get install pyqt5-dev-tools
+pip3 install pb_tool
+cd ~/.local/share/QGIS/QGIS3/profiles/default/python/plugins
+git clone https://github.com/KipCrossing/transect_aligner
+cd
+echo "QGOS is set up for dev"
